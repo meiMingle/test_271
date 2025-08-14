f8418b453c0754f54fc55b1e50816c84e64c89cb
GitHub Action <action@github.com>
2025-08-14 17:08:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/meimingle/test271/services/MyProjectService.kt
+++ src/main/kotlin/com/github/meimingle/test271/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.meimingle.test271.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meimingle.test271.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/meimingle/test271/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/meimingle/test271/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.meimingle.test271.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
